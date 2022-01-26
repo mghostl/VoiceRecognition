5b22dcd2963227d8389a5609bff57b346c37c6f2
mghostl <silverlev@Icloud.com>
2022-01-26 17:40:31 +0300

    1st lesson: custom logo, tests, textView

--- app/src/main/java/com/mghostl/education/android/voicerecognition/MainActivity.kt
+++ app/src/main/java/com/mghostl/education/android/voicerecognition/MainActivity.kt
@@ -1,10 +1,27 @@
-package com.mghostl.education.android.voicerecogniti
+package com.mghostl.education.android.voicerecognition
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val TAG = "MainActivity"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val name = "Lev"
+        val surname = "Zilberman"
+        val age = 26
+        val height = 192.0
+
+        val summary = "name: $name surname: $surname age: $age height: $height"
+        val output: TextView = findViewById(R.id.output)
+        output.text = summary
+
+        Log.d(TAG, summary)
+
     }
 }
