bc2785f02aaed76201061be45c35ec70c4079c94
mghostl <silverlev@Icloud.com>
2022-01-26 20:11:20 +0300

    2nd lesson: add menu, list, progressBar and button

---
+++ app/src/androidTest/java/com/mghostl/education/android/voicerecognition/SecondLessonInstrumentedTest.kt
@@ -0,0 +1,180 @@
+package com.mghostl.education.android.voicerecognition
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.ImageView
+import android.widget.ListView
+import androidx.annotation.DrawableRes
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.core.graphics.drawable.toBitmap
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.hamcrest.CoreMatchers.anything
+import org.hamcrest.Description
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class SecondLessonInstrumentedTest {
+    @get:Rule
+    var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun testToolbarIsDisplayed() {
+        onView(withId(R.id.toolbar))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testToolbarMenuItemStopIsDisplayed() {
+        onView(withId(R.id.action_stop))
+            .check(matches(isDisplayed()))
+    }
+
+
+    @Test
+    fun testToolbarMenuItemStopHasDrawable() {
+        onView(withId(R.id.action_stop))
+            .check(matches(menuItemWithBackgroundDrawable(R.drawable.ic_baseline_stop_circle_24)))
+    }
+
+    @Test
+    fun tetToolbarMenuItemClearIsDisplayed() {
+        onView(withId(R.id.action_clear))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testToolbarMenuItemClearHasDrawable() {
+        onView(withId(R.id.action_clear))
+            .check(matches(menuItemWithBackgroundDrawable(R.drawable.ic_baseline_clear_all_24)))
+    }
+
+    @Test
+    fun testTextInputEditTextIsDisplayed() {
+        onView(withId(R.id.text_input_edit))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testTextInputEditTextHasHint() {
+        onView(withId(R.id.text_input_edit))
+            .check(matches(withHint(R.string.request_hint)))
+    }
+
+    @Test
+    fun testTextInputEditTextHasImeAction() {
+        onView(withId(R.id.text_input_edit))
+            .check(matches(hasImeAction(EditorInfo.IME_ACTION_DONE)))
+    }
+
+    @Test
+    fun testListViewIsDisplayed() {
+            onView(withId(R.id.pods_list))
+                .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testListViewHas4Items() {
+        onView(withId(R.id.pods_list))
+            .check(matches(hasItems(4)))
+    }
+
+    @Test
+    fun testListItemHasTitle() {
+        onData(anything())
+            .inAdapterView(withId(R.id.pods_list))
+            .atPosition(0)
+            .onChildView(withId(R.id.title))
+            .check(matches(withText("Title 1")))
+    }
+
+    @Test
+    fun testListItemHasContent() {
+        onData(anything())
+            .inAdapterView(withId(R.id.pods_list))
+            .atPosition(0)
+            .onChildView(withId(R.id.content))
+            .check(matches(withText("Content 1")))
+    }
+
+    @Test
+    fun testFabIsDisplayed() {
+        onView(withId(R.id.voice_input_button))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testFabHasDrawable() {
+        onView(withId(R.id.voice_input_button))
+            .check(matches(
+                withDrawable(R.drawable.ic_baseline_keyboard_voice_24)
+            ))
+    }
+
+    private fun withDrawable(@DrawableRes id: Int) = object : TypeSafeMatcher<View>() {
+        override fun describeTo(description: Description?) {
+            description?.appendText("ImageView with drawable same as drawable with id $id")
+        }
+
+        override fun matchesSafely(item: View?): Boolean {
+            val expectedBitmap = item?.context?.getDrawable(id)?.let { getBitmapFromDrawable(it) } ?: return false
+            val actualBitmap = (item as ImageView?)?.drawable?.let { getBitmapFromDrawable(it) } ?: return false
+            return actualBitmap.sameAs(expectedBitmap)
+        }
+
+        private fun getBitmapFromDrawable(drawable: Drawable): Bitmap {
+            val bitmap = Bitmap.createBitmap(
+                drawable.intrinsicWidth,
+                drawable.intrinsicHeight,
+                Bitmap.Config.ARGB_8888
+            )
+
+            val canvas = Canvas(bitmap)
+
+            drawable.apply {
+                setBounds(0, 0, canvas.width, canvas.height)
+                setTint(Color.BLACK)
+                draw(canvas)
+            }
+            return bitmap
+        }
+    }
+
+    private fun hasItems(expectedCount: Int) = object : TypeSafeMatcher<View>() {
+        override fun describeTo(description: Description?) {
+            description?.appendText("ListView has $expectedCount items")
+        }
+
+        override fun matchesSafely(item: View?): Boolean {
+            val actualCount = (item as ListView?)?.adapter?.count ?: return false
+            return expectedCount == actualCount
+        }
+
+    }
+
+
+    private fun menuItemWithBackgroundDrawable(@DrawableRes id: Int) = object : TypeSafeMatcher<View>() {
+        override fun describeTo(description: Description?) {
+            description?.appendText("ActionMenuItemView with drawable same as drawable with id $id")
+        }
+
+        override fun matchesSafely(item: View?): Boolean {
+            val expectedBitmap = item?.context?.getDrawable(id)?.toBitmap() ?: return false
+            val actualBitmap = (item as ActionMenuItemView?)?.itemData?.icon?.toBitmap() ?: return false
+            return actualBitmap.sameAs(expectedBitmap)
+        }
+    }
+}
--- app/src/main/java/com/mghostl/education/android/voicerecognition/MainActivity.kt
+++ app/src/main/java/com/mghostl/education/android/voicerecognition/MainActivity.kt
@@ -2,26 +2,85 @@ package com.mghostl.education.android.voicerecognition
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ListView
+import android.widget.ProgressBar
+import android.widget.SimpleAdapter
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     companion object {
         const val TAG = "MainActivity"
+        const val TITLE = "Title"
+        const val CONTENT = "Content"
     }
+    
+    lateinit var requestInput: TextInputEditText
+    
+    lateinit var podsAdapter: SimpleAdapter
+
+    lateinit var progressBar: ProgressBar
+    
+    private val pods = pods()
+
+    private fun pods() = (1..4)
+        .map { podItem("Title $it", "Content $it") }
+        .toMutableList()
+
+    private fun podItem(title: String, content: String) = hashMapOf(TITLE to title, CONTENT to content)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val name = "Lev"
-        val surname = "Zilberman"
-        val age = 26
-        val height = 192.0
+        initViews()
+    }
 
-        val summary = "name: $name surname: $surname age: $age height: $height"
-        val output: TextView = findViewById(R.id.output)
-        output.text = summary
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
-        Log.d(TAG, summary)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_stop -> {
+                Log.d(TAG, "actions_stop")
+                return true
+            }
+            R.id.action_clear -> {
+                Log.d(TAG, "actions_clear")
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    private fun initViews() {
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        
+        requestInput = findViewById(R.id.text_input_edit)
+        
+        val podsList: ListView = findViewById(R.id.pods_list)
+        
+        podsAdapter = SimpleAdapter(
+            applicationContext,
+            pods,
+            R.layout.item_pod,
+            arrayOf(TITLE, CONTENT),
+            intArrayOf(R.id.title, R.id.content)
+        )
 
+        val voiceInputButton: FloatingActionButton = findViewById(R.id.voice_input_button)
+        voiceInputButton.setOnClickListener {
+            Log.d(TAG, "FAB")
+        }
+
+        progressBar  = findViewById(R.id.progress_bar)
+
+
+        podsList.adapter = podsAdapter
     }
 }
